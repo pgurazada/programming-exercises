800b5d1543693fd2cab928cbecb5e485392aa87a
Pavan Gurazada <pavankumar.gurazada@gmail.com>
2018-12-15 08:54:16 +0530

    powersets elegant code

--- src/comp-prog-book/05_search-subset-of-given-sum.jl
+++ src/comp-prog-book/05_search-subset-of-given-sum.jl
@@ -1,18 +1,14 @@
 # Given a list of n numbers and a number x, is it possible to find a subset of
 # numbers in the list that sum to x
 
-function all_subsets_of(v::Vector)
+function powerset(x::Vector{T}) where T
+    result = Vector{T}[[]]
 
-    subsets = []
-
-    max_length = length(v)
-
-    for subset_length in 1:max_length
-
-
+    for elem in x, j in eachindex(result)
+        push!(result, [result[j]; elem])
     end
 
-    return subsets
+    return result
 
 end
 
@@ -24,7 +20,25 @@ function subset_given_sum(v::Vector, x)
     left_v = v[1:mid_point]
     right_v = v[mid_point:end]
 
+    powersets_left_v = powerset(left_v)
+    powersets_right_v = powerset(right_v)
 
+    subset_sum_left_v = []
+    subset_sum_right_v= []
+
+    for subset in powersets_left_v
+        push!(subset_sum_left_v, sum(subset))
+    end
+
+    for subset in powersets_right_v
+        push!(subset_sum_right_v, sum(subset))
+    end
+
+    for subset_sum in subset_sum_left_v
+        if x - subset_sum in subset_sum_right_v
+            does_exist = true
+        end
+    end
 
     return does_exist
 
